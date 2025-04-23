ce0703497d58340cd4ecf8bbea6e299febcf59b3
mikulasmarko <5410498@upjs.sk>
2025-04-23 13:43:55 +0200

    pushik cislo 4668416 za dnesny den

--- pnmzpaz1b/src/main/java/sk/upjs/paz/Osoba2.java
+++ pnmzpaz1b/src/main/java/sk/upjs/paz/Osoba2.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class Osoba2 {
     private String meno;
-    private List<Osoba> deti = new ArrayList<>();
+    private List<Osoba2> deti = new ArrayList<>();
 
     public Osoba2(String meno) {
         this.meno = meno;
     }
 
-    public void pridajDieta(Osoba dieta) {
+    public void pridajDieta(Osoba2 dieta) {
         deti.add(dieta);
     }
 
@@ -23,11 +23,40 @@ public class Osoba2 {
         for (int i = 0; i < level; i++)
             System.out.print(' ');
         System.out.println(meno);
-        for (Osoba dieta : deti)
+        for (Osoba2 dieta : deti)
             dieta.vypisRodostrom(level + 1);
     }
 
     public int pocetSekretarok() {
-        return 0;
+        int pocet = 0;
+        for (Osoba2 osoba : deti) {
+            if (deti.size() >= 2)
+                if (osoba.deti.isEmpty()) {
+                    pocet++;
+                }
+            pocet = pocet + osoba.pocetSekretarok();
+        }
+        return pocet;
+    }
+
+    public static void main(String[] args) {
+        Osoba2 osoba = new Osoba2("sefinko");
+        Osoba2 osoba2 = new Osoba2("sekret1");
+        Osoba2 osoba3 = new Osoba2("podsefinko");
+        Osoba2 osoba4 = new Osoba2("veducko1");
+        Osoba2 osoba5 = new Osoba2("veducko2");
+        Osoba2 osoba6 = new Osoba2("veducko3");
+        Osoba2 osoba7 = new Osoba2("veducko4");
+
+
+        osoba.pridajDieta(osoba2);
+        osoba.pridajDieta(osoba3);
+        osoba3.pridajDieta(osoba4);
+        osoba3.pridajDieta(osoba5);
+        osoba3.pridajDieta(osoba6);
+        osoba6.pridajDieta(osoba7);
+        osoba.vypisRodostrom();
+
+        System.out.println(osoba.pocetSekretarok());
     }
 }
