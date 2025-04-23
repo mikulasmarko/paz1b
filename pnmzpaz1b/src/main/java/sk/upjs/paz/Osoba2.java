f2d3ce69295d0d7e60925d27effee48b5860df8e
mikulasmarko <5410498@upjs.sk>
2025-04-23 20:19:34 +0200

    ospravene sekretarky

--- pnmzpaz1b/src/main/java/sk/upjs/paz/Osoba2.java
+++ pnmzpaz1b/src/main/java/sk/upjs/paz/Osoba2.java
@@ -29,8 +29,15 @@ public class Osoba2 {
 
     public int pocetSekretarok() {
         int pocet = 0;
+        boolean mamTamNiekohoSPOdriadenymi = false;
         for (Osoba2 osoba : deti) {
-            if (deti.size() >= 2)
+
+            for (Osoba2 osoba2 : deti) {
+                if (!osoba2.deti.isEmpty()) {
+                    mamTamNiekohoSPOdriadenymi = true;
+                }
+            }
+            if (deti.size() >= 2 && mamTamNiekohoSPOdriadenymi)
                 if (osoba.deti.isEmpty()) {
                     pocet++;
                 }
@@ -46,7 +53,7 @@ public class Osoba2 {
         Osoba2 osoba4 = new Osoba2("veducko1");
         Osoba2 osoba5 = new Osoba2("veducko2");
         Osoba2 osoba6 = new Osoba2("veducko3");
-        Osoba2 osoba7 = new Osoba2("veducko4");
+//        Osoba2 osoba7 = new Osoba2("veducko4");
 
 
         osoba.pridajDieta(osoba2);
@@ -54,7 +61,7 @@ public class Osoba2 {
         osoba3.pridajDieta(osoba4);
         osoba3.pridajDieta(osoba5);
         osoba3.pridajDieta(osoba6);
-        osoba6.pridajDieta(osoba7);
+//        osoba6.pridajDieta(osoba7);
         osoba.vypisRodostrom();
 
         System.out.println(osoba.pocetSekretarok());
