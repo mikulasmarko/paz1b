715db7c956215e6f85f13082dc0b8f7a582b8376
mikulasmarko <5410498@upjs.sk>
2025-05-10 12:06:23 +0200

    velky vlakovy problem plus nejaky koment

--- palma01/src/main/java/sk/upjs/paz/source.java
+++ palma01/src/main/java/sk/upjs/paz/source.java
@@ -9,10 +9,11 @@ import java.io.InputStreamReader;
 public class source {
 
 
-    public void nacitajSubor() {
+    public void vyriesUlohu() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int pocetVlakov = Integer.parseInt(br.readLine());
 
+            //prechadzam vlak po vlaku
             for (int i = 0; i < pocetVlakov; i++) {
                 System.out.println(vyriesVlak(br.readLine()));
             }
@@ -25,38 +26,47 @@ public class source {
     public int vyriesVlak(String string) {
         String[] riadok = string.split(" ");
 
-        int sprievodcovia = Integer.parseInt(riadok[0]);
-        int vozne = Integer.parseInt(riadok[1]);
+        int pocetSprievodcov = Integer.parseInt(riadok[0]);
+        int pocetVoznov = Integer.parseInt(riadok[1]);
 
-        int[] pasazieriVoVoznoch = new int[vozne];//velkost 9 v prvom pripade
-        int priemer = 0;
+        int[] poctyPasazierovVoVoznoch = new int[pocetVoznov];//velkost 9 v prvom pripade
+        int priemerNaSprievodcu = 0;
 
-        for (int i = 0; i < vozne; i++) {
-            int aktualnyPocetVoVozni = Integer.parseInt(riadok[i + 2]);
-            pasazieriVoVoznoch[i] = aktualnyPocetVoVozni;
-            priemer += aktualnyPocetVoVozni;
+        for (int i = 0; i < pocetVoznov; i++) {
+            int aktualnyVozen = Integer.parseInt(riadok[i + 2]);
+            poctyPasazierovVoVoznoch[i] = aktualnyVozen;
+            //pripocitam pocet ludi vo vozni k priemeru - zatial iba pocet vsetkych ludi vo voznoch
+            priemerNaSprievodcu += aktualnyVozen;
         }
 
-//        System.out.println(Arrays.toString(pasazieriVoVoznoch));
-//        System.out.println(priemer);
-        priemer = (int) priemer / sprievodcovia;
-//        System.out.println(priemer);
+//        System.out.println(Arrays.toString(poctyPasazierovVoVoznoch));
+//        System.out.println(priemerNaSprievodcu);
+        //vypocitam priemernu hodnotu
+        priemerNaSprievodcu = (int) priemerNaSprievodcu / pocetSprievodcov;
+//        System.out.println(priemerNaSprievodcu);
 
+
+        //vytvorim si pocitadlo kde budem pripocitavat hodnotu pre
         int pocitadlo = 0;
-        int ktorySprievodca = 0;
-        int[] sprievodcoviaPole = new int[sprievodcovia];
+        //urcujem ktoremu sprievodcovi dam hodnotu, vypocitam, priradim index na ktory budem davat o jeden vacsi
+        int idxSprievodcu = 0;
+        //vytvorim si pole sprievodcov do ktoreho si ulozim kolkych po sebe ludi mal jeden sprievodca
+        int[] sprievodcoviaPole = new int[pocetSprievodcov];
 
-        for (int i = 0; i < pasazieriVoVoznoch.length - 1; i++) {
-            pocitadlo += pasazieriVoVoznoch[i];
-            if ((pocitadlo > priemer || pocitadlo + pasazieriVoVoznoch[i + 1] / 2 > priemer) && ktorySprievodca < sprievodcovia - 1) {
-                sprievodcoviaPole[ktorySprievodca] = pocitadlo;
-                ktorySprievodca++;
+        //prechadzam vozen po vozni - chcem prejst vsetky, musim zapocitat kazdeho zakaznika
+        for (int i = 0; i < poctyPasazierovVoVoznoch.length - 1; i++) {
+            pocitadlo += poctyPasazierovVoVoznoch[i];
+            //ak sa priblizim k hranici alebo som ju prekrocil, zapisem sprievodcovi pocet, posuniem sprievodcu, vynulujem pocitadlo
+            if ((pocitadlo > priemerNaSprievodcu || pocitadlo + poctyPasazierovVoVoznoch[i + 1] / 2 > priemerNaSprievodcu) && idxSprievodcu < pocetSprievodcov - 1) {
+                sprievodcoviaPole[idxSprievodcu] = pocitadlo;
+                idxSprievodcu++;
                 pocitadlo = 0;
             }
         }
-        pocitadlo += pasazieriVoVoznoch[vozne - 1];
-        sprievodcoviaPole[sprievodcovia - 1] = pocitadlo;
-
+        //vyriesim aj posledny vozen
+        pocitadlo += poctyPasazierovVoVoznoch[pocetVoznov - 1];
+        sprievodcoviaPole[pocetSprievodcov - 1] = pocitadlo;
+        //zistim kolko max ludi bude mat sprievodca na krku
         int max = 0;
         for (int j : sprievodcoviaPole) {
             if (j > max) {
@@ -68,6 +78,6 @@ public class source {
 
     public static void main(String[] args) {
         source s = new source();
-        s.nacitajSubor();
+        s.vyriesUlohu();
     }
 }
